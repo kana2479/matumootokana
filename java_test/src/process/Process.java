779d6943e0fe2cda68a83caa3719397df98bb3bf
mizuki <mizuki@192.168.3.4>
2024-01-18 03:23:05 +0900

    1_22新規提出

---
+++ java_test/src/process/Process.java
@@ -0,0 +1,22 @@
+package process;
+
+public class Process {
+	
+
+	public String hello;
+	public String japan;
+	public String susi;
+	public String delicious;
+	public String food;
+	public String time;
+	
+	public Process(String hello, String japan, String susi, String delicious, String food, String time) {
+		this.hello = hello;
+		this.japan = japan;
+		this.susi = susi;
+		this.delicious = delicious;
+		this.food = food;
+		this.time= time;
+
+	}
+}
