291ae92b87c9036b7edcdf420029d703c43be2f2
kana <kana@localhost>
2024-06-16 16:25:02 +0900

    新規提出1-30

--- java_sample/src/selfintroductionProgram/Main.java
+++ java_sample/src/selfintroductionProgram/Main.java
@@ -3,7 +3,7 @@ package selfintroductionProgram;
 public class Main {
 	 public static void main(String[] args) {
 		    Person person1 = new Person("鈴木太郎", 20, 1.7,60);
-		    person1.print();
+		   
 		    
 		    System.out.println(person1.name);
 		    System.out.println(person1.age);
--- java_sample/src/selfintroductionProgram/Person.java
+++ java_sample/src/selfintroductionProgram/Person.java
@@ -7,25 +7,25 @@ public class Person {
 	public double height;
 	public double weight;
 	public double bmi;
-	
-	
+
 	// コンストラクタを定義しインスタンスフィールドに値をセット
-	Person(String name,	int age,double height,double weight){
-		this.name=name;
-		this.age=age;
-		this.height=height;
-		this.weight=weight;
-		
+	Person(String name, int age, double height, double weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+
 	}
-	public double bmi(){
-		return this.weight / this.height  / this.height;
-			}
-	
+
+	public double bmi() {
+		return this.weight / this.height / this.height;
+	}
+
 	public void print() {
-		 System.out.println("名前は"+this.name+"です");
-		 System.out.println("年は"+this.age+"です");
-		 System.out.println("BMIは"+(Math.floor(this.bmi()))+"です");
-		 
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age + "です");
+		System.out.println("BMIは" + (Math.floor(this.bmi())) + "です");
+
 	}
-	
+
 }
