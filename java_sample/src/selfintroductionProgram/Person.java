56082289aa0e01d809896ea4ddc9071175133da5
kana <kana@localhost>
2024-06-16 16:53:55 +0900

    新規提出1-32

--- java_sample/src/selfintroductionProgram/Person.java
+++ java_sample/src/selfintroductionProgram/Person.java
@@ -2,7 +2,8 @@ package selfintroductionProgram;
 
 public class Person {
 	// インスタンスフィールドを定義
-	public String name;
+	public String firstName;
+	public String lastName;
 	public int age;
 	public double height;
 	public double weight;
@@ -10,23 +11,35 @@ public class Person {
 	public static int count=0;
 
 	// コンストラクタを定義しインスタンスフィールドに値をセット
-	Person(String name, int age, double height, double weight) {
-		this.name = name;
+	Person(String firstName,int age, double height, double weight) {
+		this.firstName = firstName;
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
 		Person.count++;
 
 	}
+	Person(String firstName,String lastName,int age, double height, double weight) {
+		this.firstName = firstName;
+		this.lastName =lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		Person.count++;
+	}
+	
+	public String fullName(){
+		return this.firstName + this.lastName;
+		}
 
 	public double bmi() {
 		return this.weight / this.height / this.height;
 	}
 
 	public void print() {
-		System.out.println("名前は" + this.name + "です");
+		System.out.println("名前は" + this.firstName + "です");
 		System.out.println("年は" + this.age + "です");
-		System.out.println("BMIは" + (Math.floor(this.bmi())) + "です");
+		
 
 	}
 	public static void printCount() {
